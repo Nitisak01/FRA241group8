6af79c511cd7f1b182f7225d17491ed221a3c40e
champ <champnarak15@hotmail.com>
2016-11-27 05:52:17 +0700

    sign up page

--- client/app/app.routing.ts
+++ client/app/app.routing.ts
@@ -1,22 +1,22 @@
-import { ModuleWithProviders } from '@angular/core';
+import  { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {logincomponent} from "./login.component";
 import {searchcomponent} from "./search.component";
 import {floorsearchcomponent} from "./floorsearch.component";
 import {datesearchcomponent} from "./datesearch.component";
 import {techcomponent} from "./tech.component";
-
+import {SignupComponent} from "./sign_up/sign_up.component";
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { FormComponent }  from './form/form.component';
-
 const appRoutes: Routes = [
-    // { path: '', component: logincomponent },
+
     { path: 'login', component: logincomponent },
     { path: 'search', component: searchcomponent },
     { path: 'search/floor', component: floorsearchcomponent},
     { path: 'search/date', component: datesearchcomponent},
     { path: 'tech', component: techcomponent},
+    { path: 'signup', component: SignupComponent},
     { path: 'book', component: Page2Component },
     { path: 'book/form', component: Page1Component },
     { path: 'book/form/commitform', component: FormComponent },
